7aa62243c61f778f2369e93e14a900487c3aec9a
dhirajhanagal007-eng <dhirajhanagal007@gmail.com>
2026-09-02 14:25:01 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,6 @@
+firstname = input("Enter your first name")
+lastname = input("Enter your last name")
+city = input("Enter your city")
+
+print("User first name and last name ", firstname + " " + lastname)
+print("User city", city)
